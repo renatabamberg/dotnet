b759f6690192b75897b867b8e07cd8b82ca47eee
Renata <rbamberg6@gmail.com>
2023-11-07 17:12:05 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+#region TiposDados
+  int tipoInteiro;
+  double tipoDouble;
+  decimal tipoDecimal;
+  long tipoLong;
+
+  tipoInteiro = int.MaxValue;
+  
+  Console.WriteLine($"Este valor {tipoInteiro} eh o maior inteiro possivel");
+
+#endregion
